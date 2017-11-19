201c306d679c9554ac0e61a989b247ddcc8cad6f
MYTH-INF <maytheinfante96@gmail.com>
2017-11-19 10:09:34 -0500

    actualizando archivo js

--- js/app.js
+++ js/app.js
@@ -4,6 +4,36 @@ window.addEventListener('load', function(event) {
   var textContainer = document.getElementById('text-container');
 
   button.addEventListener('click', addCharacters);
+  characters.addEventListener('keyup', countCharacters);
+
+  //  creando función que cuenta n° de caracteres ingresados
+  function countCharacters() {
+    var numberCharacters = document.getElementById('number_characters');
+    var size = characters.value.length;
+    var max = 140;
+    var finalSize = numberCharacters.textContent = (max - size);
+
+    // deshabilitando botón y cambiar de color si los caracteres pasan los 140
+    if (finalSize <= 0) {
+      button.disabled = true;
+      numberCharacters.style.color = 'red';
+
+    // cambiar contador a color morado si hay más de 120 caracteres
+    } else if (finalSize <= 20 && finalSize > 10) {
+      button.disabled = false;
+      numberCharacters.style.color = 'purple';
+    // cambiar contador a color fucsia si hay más de 130 caracteres
+    } else if (finalSize < 10 && finalSize >= 0) {
+      button.disabled = false;
+      numberCharacters.style.color = '#FF1493';
+    } else if (finalSize < 0) {
+      button.disabled = false;
+      numberCharacters.style.color = 'red';
+    } else {
+      button.disabled = false;
+      numberCharacters.style.color = '#7788' ;
+    }
+  };
   //  creando función que almacena los textos a un contenetor
   function addCharacters() {
     if (characters.value) {
@@ -34,34 +64,5 @@ window.addEventListener('load', function(event) {
     function pressEnter() {
       characters.style.cssText = 'height:' + characters.scrollHeight + 'px';
     }
-    characters.addEventListener('keyup', countCharacters);
-    //  creando función que cuenta n° de caracteres ingresados
-    function countCharacters() {
-      var numberCharacters = document.getElementById('number_characters');
-      var size = characters.value.length;
-      var max = 140;
-      var finalSize = numberCharacters.textContent = (max - size);
-
-      // deshabilitando botón y cambiar de color si los caracteres pasan los 140
-      if (finalSize <= 0) {
-        button.disabled = true;
-        numberCharacters.style.color = 'red';
-
-      // cambiar contador a color morado si hay más de 120 caracteres
-      } else if (finalSize <= 20 && finalSize > 10) {
-        button.disabled = false;
-        numberCharacters.style.color = 'purple';
-      // cambiar contador a color fucsia si hay más de 130 caracteres
-      } else if (finalSize < 10 && finalSize >= 0) {
-        button.disabled = false;
-        numberCharacters.style.color = '#FF1493';
-      } else if (finalSize < 0) {
-        button.disabled = false;
-        numberCharacters.style.color = 'red';
-      } else {
-        button.disabled = false;
-        numberCharacters.style.color = '#7788' ;
-      }
-    };
   };
 });
