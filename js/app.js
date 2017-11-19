e076549abd3f377e49c102a4e04db8d675b6c61d
MYTH-INF <maytheinfante96@gmail.com>
2017-11-19 01:21:39 -0500

    agregando versión 0.0.3

--- js/app.js
+++ js/app.js
@@ -20,14 +20,35 @@ window.addEventListener('load', function(event) {
       textDiv.appendChild(text);
       characters.value = '';
     };
-  };
-  characters.addEventListener('keyup', countCharacters);
-  //  creando función que cuenta n° de caracteres ingresados
-  function countCharacters() {
-    var numberCharacters = document.getElementById('number_characters');
-    var size = characters.value.length;
-    var max = 140;
-    numberCharacters.textContent = (max - size);
+
+    characters.addEventListener('keyup', countCharacters);
+    //  creando función que cuenta n° de caracteres ingresados
+    function countCharacters() {
+      var numberCharacters = document.getElementById('number_characters');
+      var size = characters.value.length;
+      var max = 140;
+      var finalSize = numberCharacters.textContent = (max - size);
+
+      // deshabilitando botón y cambiar de color si los caracteres pasan los 140
+      if (finalSize <= 0) {
+        button.disabled = true;
+        numberCharacters.style.color = 'red';
+
+      // cambiar contador a color morado si hay más de 120 caracteres
+      } else if (finalSize <= 20 && finalSize > 10) {
+        button.disabled = false;
+        numberCharacters.style.color = 'purple';
+      // cambiar contador a color fucsia si hay más de 130 caracteres
+      } else if (finalSize < 10 && finalSize >= 0) {
+        button.disabled = false;
+        numberCharacters.style.color = '#FF1493';
+      } else if (finalSize < 0) {
+        button.disabled = false;
+        numberCharacters.style.color = 'red';
+      } else {
+        button.disabled = false;
+        numberCharacters.style.color = '#7788' ;
+      }
+    };
   };
 });
-//  countCharacters.value
